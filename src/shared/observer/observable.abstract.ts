32f0c0d5861bde6828539d4fa2bcab70fb8094ff
Eric Santana <efms.king@gmail.com>
2026-08-26 19:56:30 -0300

    :sparkles: feat: add observer and observables interface and class respectively

---
+++ src/shared/observer/observable.abstract.ts
@@ -0,0 +1,13 @@
+import { Observer } from "./observer.interface";
+
+export abstract class Observable<TObserver extends Observer<TData>, TData> {
+
+    private subscribers: Array<TObserver> = [];
+    
+    subscribe(observer:TObserver): void {
+        this.subscribers.push(observer);
+    }
+    protected notify(data: TData) {
+        this.subscribers.forEach(s => s.update(data))
+    }
+}
