f660157cae490a0398cbf6b459f001ecc65ba87c
Eric Santana <efms.king@gmail.com>
2026-08-29 16:15:16 -0300

    chore: change from ts-node to tsx

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import Fastify from "fastify";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import pickupRoutes from "./src/pickup/pickup.routes.ts";
 
 const server = Fastify({
   logger: true,
 });
 
-server.register(pickupRoutes);
+server.register(pickupRoutes, {prefix: '/api'});
+
+server.get('/', (_request: FastifyRequest, _reply: FastifyReply) => "server up!")
 
 server.listen({ port: 8080 }, (err, address) => {
   if(err) {
